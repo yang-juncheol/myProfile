0c7fdf432dcb114489f30dd51815be4955a6ef91
Juncheol <Juncheol99481201@gmail.com>
2024-01-02 12:09:38 +0900

    수정

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,20 +1,28 @@
-import { useState } from 'react';
-import MyProfile from './MyProfile';
-import Navbar from './Navbar';
+import { useState } from "react";
+import MyProfile from "./MyProfile";
+import Navbar from "./Navbar";
 
-import style from './myComponent.module.css';
-
+import style from "./myComponent.module.css";
 
 function MyComponent() {
-    return <div className={style.myComponent}>
-        <header className={style.myComponentHeader}>
-            <Navbar />
-        </header>
-        <div className={style.myComponentBody}>
-            <h1>내 프로필을 검색해보자!</h1>
-            <MyProfile />
-        </div>
-    </div>;
+  var [value, setValue] = useState("");
+  return (
+    <div className={style.myComponent}>
+      <header className={style.myComponentHeader}>
+        <Navbar onChange={(value) => setValue(value)} />
+      </header>
+      <div className={style.myComponentBody}>
+        <h1>내 프로필을 검색해보자!</h1>
+        {value === "양준철" ? (
+          <MyProfile />
+        ) : value === "" ? (
+          <p>상단 검색바에서 자신의 이름을 검색해 보세요</p>
+        ) : (
+          <p>찾을 수 없습니다</p>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default MyComponent;
--- src/MyProfile.js
+++ src/MyProfile.js
@@ -1,27 +1,29 @@
 import style from "./myProfile.module.css";
 
 function MyProfile() {
-    return <div className={style.profileContiner}>
-        <img src={`${process.env.PUBLIC_URL}/profile_image.jpg`} />
-        <ul className={style.profileInfo}>
-            <li className={style.profileRow}>
-                <h5>이름</h5>
-                <p>정택유</p>
-            </li>
-            <li className={style.profileRow}>
-                <h5>나이</h5>
-                <p>27</p>
-            </li>
-            <li className={style.profileRow}>
-                <h5>직업</h5>
-                <p>프톤트개발자</p>
-            </li>
-            <li className={style.profileRow}>
-                <h5>직장</h5>
-                <p>투네이션</p>
-            </li>
-        </ul>
-    </div>;
+  return (
+    <div className={style.profileContiner}>
+      <img src={`${process.env.PUBLIC_URL}/다운로드.jpeg`} />
+      <ul className={style.profileInfo}>
+        <li className={style.profileRow}>
+          <h5>이름</h5>
+          <p>양준철</p>
+        </li>
+        <li className={style.profileRow}>
+          <h5>나이</h5>
+          <p>19</p>
+        </li>
+        <li className={style.profileRow}>
+          <h5>직업</h5>
+          <p>프톤트개발자</p>
+        </li>
+        <li className={style.profileRow}>
+          <h5>직장</h5>
+          <p>삼성</p>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default MyProfile;
